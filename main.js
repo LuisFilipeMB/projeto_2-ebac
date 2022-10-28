c8d4910bfb3738926795edebca2cb8eb81dd49d8
Luis Filipe <luis.miake@gmail.com>
2022-10-28 12:16:08 -0300

    fix: fix decimal numbers

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function calculaMediafinal(){
 function atualizaMédiaFinal(){
     const mediaFinal = calculaMediafinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinina ? spanAprovado : spanReprovado;
 }
 
